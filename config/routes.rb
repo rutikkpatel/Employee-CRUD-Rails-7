8a0dd39e07b7ce7e60653ae150c528640a6caffd
rutikkpatel <rutikkpatel@gmail.com>
2023-02-12 22:29:45 +0530

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'employees/index'
-  get 'employees/new'
-  get 'employees/create'
-  get 'employees/show'
-  get 'employees/edit'
-  get 'employees/update'
-  get 'employees/destroy'
+  resources :employees
+  root "employees#index"
 end
